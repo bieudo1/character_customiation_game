9e61a91b5ed574d33c139b5d22c5daf1079228e7
bieudo1 <103914756+bieudo1@users.noreply.github.com>
2022-06-06 11:48:39 +0700

    bài làm

---
+++ src/components/Part.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Part = ({ path, index, zIndex }) => {
+	return <img src={`character/${path}/${index + 1}.png`} alt="" width={260} style={{ zIndex, position: 'absolute', left: 0, top: 0 }} />;
+};
